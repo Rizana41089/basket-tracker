9f370f907c9556e1e858d3c9a2f9f108f19fe57b
Rizana41089 <93428760+Rizana41089@users.noreply.github.com>
2026-01-01 19:05:42 +0700

    app.py

--- app.py
+++ app.py
@@ -18,14 +18,11 @@ def load_data():
 def save_data(df):
     df.to_csv(DATA_FILE, index=False)
 
-# --- STYLE CSS ------------------------------------------------------
+# --- STYLE CSS (Biar Tampilan Rapi) ---------------------------------
 st.markdown("""
     <style>
-    /* .stApp {max-width: 600px; margin: 0 auto;}  <-- INI YG BIKIN SEMPIT, KU HAPUS */
+    .stApp {max-width: 600px; margin: 0 auto;}
     .block-container {padding-top: 2rem; padding-bottom: 5rem;}
-    /* Sembunyikan tombol menu default Streamlit di pojok kanan atas biar bersih */
-    #MainMenu {visibility: hidden;}
-    footer {visibility: hidden;}
     </style>
     """, unsafe_allow_html=True)
 
